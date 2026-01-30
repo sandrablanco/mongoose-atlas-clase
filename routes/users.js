1a6ca44ab786e1e9330bbef112007f9f20c5774f
Sandra Blanco <sandra.blanco18@gmail.com>
2026-01-30 18:16:05 +0100

    renombre de carpetas y variables

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User.js"); 
+const user = require("../models/user.js"); 
 
 
 router.post("/create", async(req, res) => {
